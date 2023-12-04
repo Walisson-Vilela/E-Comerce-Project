64a670a2847098364b26ffbf1c6493f5aa4620ca
Vilela Walisson <QG6251@engie.com>
2023-12-04 12:54:47 -0300

    Atualizacao do codigo e criacao de props
    Atualizacao do codigo e criacao de props

--- e-commerce/src/components/Header/index.tsx
+++ e-commerce/src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-const Header = () => {
 
+interface IProps {
+    name: string;
+}
+
+const Header = ({ name }: IProps) => {
 
     return (
 
         <nav className='w-full flex justify-between items-center p-5 bg-white shadow-xl'>
-            <span>Nome da Compania</span>
+            <span>{name}</span>
             <ul className='flex items-center'>
                 <li className='mx-2'>Features</li>
                 <li className='mx-2'>Premium</li>
