10302a8d25c9df6a314df84ed35c2c281968c019
Walisson Vilela <59664662+Walisson-Vilela@users.noreply.github.com>
2024-04-25 21:43:59 -0300

    [CONFIG] = Configuracao do tailwind
    Configuracao do tailwind

--- e-commerce/src/components/Header/index.tsx
+++ e-commerce/src/components/Header/index.tsx
@@ -1,26 +1,22 @@
-
 interface IProps {
     name: string;
 }
 
 const Header = ({ name }: IProps) => {
-
     return (
-
-        <nav className='w-full flex justify-between items-center p-5 bg-white shadow-xl'>
-            <span>{name}</span>
+        <nav className='w-full flex justify-between items-center p-5 border-b border-stone bg-gray-700'>
+            <span className='mx-2 text-white'>{name}</span>
             <ul className='flex items-center'>
-                <li className='mx-2'>Features</li>
-                <li className='mx-2'>Premium</li>
-                <li className='mx-2'>Suporte</li>
+                <li className='mx-2 text-white'>Features</li>
+                <li className='mx-2 text-white'>Premium</li>
+                <li className='mx-2 text-white'>Suporte</li>
                 <li>
-                    <button>
+                    <button className='text-grey-700' >
                         Cadastro
                     </button>
                 </li>
-            </ul >
-        </nav >
-
+            </ul>
+        </nav>
     );
 }
 
